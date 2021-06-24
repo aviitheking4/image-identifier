99b05260f9bb51385d768e5fbe34808a9d7d1d54
aviitheking4 <78423799+aviitheking4@users.noreply.github.com>
2021-06-24 18:44:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,36 @@ Webcam.set({
         facingMode: " enviroment"
     }
 });
-camera = document.getElementById("camer");
+camera = document.getElementById("camera");
 
 Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (errror) {
+        console.error(error)
+    }else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
